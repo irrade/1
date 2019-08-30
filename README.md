17e9a468e58bf15528ff7de5877dd81ece703b8f
irrade <33498016+irrade@users.noreply.github.com>
2019-08-30 17:02:42 +0300

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,32 @@
-# 1
+**AppFunc**
+
+
+This app include these functions:
+1.	increment()
+Adds a search to a collection. The search term should be assigned randomly from the attached list of search terms.
+2.	num_last_minute()
+Queries the collection to return the number of searches made in the past minute (Hint: this can be done in O(log(n))).
+3.	num_arbitrary_lookback(seconds)
+Given a number of seconds returns the number of searches made in that time interval (should be an abstraction of Q2).
+4.	most_common_term(seconds)
+Given an arbitrary lookback, find the most commonly searched term.
+
+Global variables in app_funcs.py are:
+
+PATH = 'c:\IKI\_test_conductor'
+STORAGE_FILE_NAME = 'data_storage.json'
+TERMS_FILE_NAME = 'phrases_ns.csv'
+
+You can change them according to your preferences.
+
+Before run functions 2-4, you should run func 1 to generate data storage  data from which will be used for further calculation.
+You can run the app from command line, just initialize the path to the project location and activate evn conductor_test. 
+Requirements are in requirements.txt. 
+Then you can call all of the functions from app_funcs.py.
+
+
+The data structure is list of dictionaries and stored in JSON format in the project folder.
+If searches are frequent, I consider to collect load them to json, it can tke time.
+Probably is better to collect them to list for some time and then add it to JSON.
+Basically, generators are better choice in this case.
+
---
+++ app_funcs.py
@@ -0,0 +1,135 @@
+import uuid
+import random
+from datetime import datetime, timedelta
+import pandas as pd
+import click
+import sys
+import json
+import os
+
+PATH = 'c:\IKI\_test_conductor'
+STORAGE_FILE_NAME = 'data_storage.json'
+TERMS_FILE_NAME = 'phrases_ns.csv'
+
+
+def _download_terms(path):
+    """
+    This func to download terms for random assignment
+    :type path: path where the file with term is located
+    """
+    terms = pd.read_csv(os.path.join(path, TERMS_FILE_NAME), header=None, skiprows=1)
+    terms_l = terms[0].tolist()
+    return terms_l
+
+
+def _append_to_data_storage(entry):
+    """
+    This func add search to data_storage
+    :param entry: new search
+    :return: add new serach
+    """
+    path_ = os.path.join(PATH, STORAGE_FILE_NAME)
+    if not os.path.isfile(path_):
+        with open(path_, 'w') as f:
+            f.write(json.dumps([]))
+
+    with open(path_, 'ab+') as f:
+        f.seek(0, 2)
+        if f.tell() == 0:
+            f.write(json.dumps([entry]).encode())
+        else:
+            f.seek(-1, 2)
+            f.truncate()
+            f.write(','.encode())
+            f.write(json.dumps(entry).encode())
+            f.write(']'.encode())
+
+           
+def _load_data_storage():
+    path_ = os.path.join(PATH, STORAGE_FILE_NAME)
+    with open(path_, 'r') as f:
+        dicts = json.load(f)
+    return dicts
+    
+
+
+def _date_range(term_datetime, time_measurement, value):
+    """
+    this func check whether the datetime is in range based on minutes or seconds
+    :return: true or false
+    """
+    timedelta_ = timedelta(seconds=value) if time_measurement == 'seconds' else timedelta(minutes=value)
+    end_date = datetime.now()
+    start_date = end_date - timedelta_
+    condition = start_date <= term_datetime <= end_date
+    return condition
+
+
+def _count_terms(time_type, value_=1):
+    """
+    this func is used for num_arbitrary_lookback(seconds) and num_last_minute()
+    :param time_type: seconds or minutes
+    :param value_: should be int which refer to minutes or seconds
+    :return:
+    """
+    time_ = 'minutes' if time_type == 'minutes' else 'seconds'
+    dicts = _load_data_storage()
+    counter = 0
+    for current_dict in dicts:
+        term_datetime = datetime.fromtimestamp(current_dict['timestamp'])
+        date_range = _date_range(term_datetime=term_datetime, time_measurement=str(time_), value=int(value_))
+        if date_range:
+            counter += 1
+    return counter
+
+
+@click.group()
+def main():
+    pass
+
+
+@click.option('--path_', default=PATH, help='Add the path to the file of terms location')
+@main.command()
+def increment(path_=PATH):
+#    dicts = []
+    terms_l = _download_terms(path_)
+    for i in range(0,len(terms_l)):
+        entry = {'term': str(random.choice(terms_l)), 'id': str(uuid.uuid1()), 'timestamp': datetime.now().timestamp()}
+        _append_to_data_storage(entry)
+    print('Data generated')
+    sys.exit(0)
+
+
+@main.command()
+def num_last_minute():
+    counter = _count_terms(time_type='minutes', value_=1)
+    print(counter)
+    sys.exit(0)
+
+
+@click.argument('seconds')
+@main.command()
+def num_arbitrary_lookback(seconds):
+    counter = _count_terms(time_type='seconds', value_=int(seconds))
+    print(counter)
+    sys.exit(0)
+
+
+@click.argument('seconds')
+@click.option('--dicts', default=_load_data_storage(), help='Add dictionary')
+@main.command()
+def most_common_term(seconds, dicts=_load_data_storage()):
+    # dicts = _load_data_storage()
+    terms = []
+    for current_dict in dicts:
+        term_datetime = datetime.fromtimestamp(current_dict['timestamp'])
+        date_range = _date_range(term_datetime=term_datetime, time_measurement='seconds', value=int(seconds))
+        if date_range:
+            terms.append(current_dict['term'])
+    term = max(set(terms), key=terms.count)
+    print(term)
+    sys.exit(0)
+
+
+if __name__ == "__main__":
+    main()
